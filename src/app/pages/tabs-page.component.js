79d2345b8cfc0d56277c6b0bb5ffe8e8f96e6298
fokusfm@yandex.ru <fokusfm@yandex.ru>
2017-08-27 18:06:53 +0300

    Tab page. Implement tab click

--- src/app/pages/tabs-page.component.js
+++ src/app/pages/tabs-page.component.js
@@ -4,6 +4,20 @@ class TabsPageComponent extends WFMComponent {
     constructor(config) {
         super(config);
     }
+
+    events(){
+      return {
+        'click .collapsible':'onTabClick'
+      }
+    }
+
+    onTabClick({ target }){
+      if(!target.classList.contains('collapsible-header')) { return; }
+
+      this.el.querySelectorAll('.js-tab').forEach(el => {el.classList.remove('active'); });
+
+      target.parentNode.classList.add('active');
+    }
 }
 
 export const tabsPageComponent = new TabsPageComponent({
@@ -14,15 +28,15 @@ export const tabsPageComponent = new TabsPageComponent({
         <div class="row">
         <div class ="col s6 offset-s3">
         <ul class="collapsible popout collapsible-accordion">
-          <li >
+          <li class="js-tab">
             <div class="collapsible-header"><i class="material-icons">subtitles</i>First</div>
             <div class="collapsible-body" ><span>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</span></div>
           </li>
-          <li >
+          <li class="js-tab">
             <div class="collapsible-header"><i class="material-icons">games</i>Second</div>
             <div class="collapsible-body" ><span>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</span></div>
           </li>
-          <li class="active">
+          <li class="js-tab active">
             <div class="collapsible-header "><i class="material-icons">library_books</i>Third</div>
             <div class="collapsible-body" ><span>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</span></div>
           </li>
--- src/framework/core/component.js
+++ src/framework/core/component.js
@@ -11,5 +11,21 @@ export class Component {
             throw new Error(`The component with selector ${this.selector} wasn't find!`);
         }
         this.el.innerHTML = this.template;
+
+        this._initEvents();
+    }
+
+    _initEvents(){
+        if(!this.events){ return; }
+
+        let events = this.events()
+        Object.keys(events).forEach(key => {
+            var listenerArr = key.split(' '); 
+            var eventName = listenerArr[0]; 
+            var selector = listenerArr[1];
+            this.el
+            .querySelector(selector)
+            .addEventListener(eventName, this[events[key]].bind(this))
+        });
     }
 }
